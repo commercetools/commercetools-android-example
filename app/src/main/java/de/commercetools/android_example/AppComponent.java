991e3ad5a9e3aa003fd1013509830dab7ba74ae1
schleichardt <michael.schleichardt@commercetools.de>
2017-06-29 16:54:10 +0200

    rename to JsonModule

--- app/src/main/java/de/commercetools/android_example/AppComponent.java
+++ app/src/main/java/de/commercetools/android_example/AppComponent.java
@@ -6,7 +6,7 @@ import dagger.Component;
 import dagger.android.AndroidInjectionModule;
 
 @Singleton
-@Component(modules={AndroidInjectionModule.class, AppModule.class, CartActivityModule.class, StuffModule.class})
+@Component(modules={AndroidInjectionModule.class, AppModule.class, CartActivityModule.class, JsonModule.class})
 public interface AppComponent {
     void inject(CtApp app);
 }
